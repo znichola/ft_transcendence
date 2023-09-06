2461b673a30d9745115cf483065089b247f0798b
Nicholas Zivkovic <znichola@student.42lausanne.ch>
2023-09-06 10:59:37 +0200

    spread and arrow functions

--- react-app/src/NikiTickTackToe.tsx
+++ react-app/src/NikiTickTackToe.tsx
@@ -11,26 +11,27 @@ import { useState } from "react";
 // }
 
 export default function Board() {
-  // const [squares, setSquares] = useState(Array(9).fill(""));
+  const [squares, setSquares] = useState([
+    ...Array.from({ length: 10 }, (_, i) => (i + 1).toString()),
+  ]);
   
-
   return (
     <>
       <div className="">
-        <Square value={"1"} />
-        <Square value={"2"} />
-        <Square value={"3"} />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
 
       <div>
-        <Square value={"4"} />
-        <Square value={"5"} />
-        <Square value={"6"} />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div>
-        <Square value={"7"} />
-        <Square value={"8"} />
-        <Square value={"9"} />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
