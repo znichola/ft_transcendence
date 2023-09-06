9f1b464dba5a5d4efd5ddb864f5043742ef3fd63
Nicholas Zivkovic <znichola@student.42lausanne.ch>
2023-09-06 10:17:08 +0200

    before state refactor to put it into board
    following the tutorial, the state is refactored to live inside
    board and not the individual square following the react phylosophy

--- react-app/src/NikiTickTackToe.tsx
+++ react-app/src/NikiTickTackToe.tsx
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 // function Square({
 //   value,
@@ -11,6 +11,9 @@
 // }
 
 export default function Board() {
+  // const [squares, setSquares] = useState(Array(9).fill(""));
+  
+
   return (
     <>
       <div className="">
@@ -34,9 +37,22 @@ export default function Board() {
 }
 
 function Square({ value }: { value: string }) {
+  const [svalue, setSValue] = useState<string>(); // explicit type is declared with the <>
+  // const [svalue, setSValue] = useState("");    // type is infered from the arguemnt ""
+
+  function handleClick() {
+    console.log("clicked!");
+    setSValue(svalue ? "" : "X");
+  }
+
   return (
-    <button className=" text-5xl font-bold text-slate-700 border-4 border-slate-700 h-20 w-20 hover:border-red-400 m-1 bg-amber-50">
-      {value}
+    <button
+      onClick={handleClick}
+      className={`text-5xl font-bold ${
+        svalue ? "text-slate-700" : "text-stone-300"
+      } border-4 border-slate-700 h-20 w-20 hover:border-red-400 m-1 bg-amber-50`}
+    >
+      {svalue || value}
     </button>
   );
 }
