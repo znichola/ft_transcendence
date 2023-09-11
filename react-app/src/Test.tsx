422522bcb612ffdc26fc48ea395518b47d3a61c6
Nicholas Zivkovic <znichola@student.42lausanne.ch>
2023-09-11 09:05:47 +0200

    example get with axios and RQ working

--- react-app/src/Test.tsx
+++ react-app/src/Test.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { LoadingSpinnerMessage } from "./components";
 import { fetchAllUsers, fetchTodoList, fetchUser } from "./api";
-import axios, { isCancel, AxiosError } from "axios";
+import axios from "axios";
 
 import { Avatar } from "./Profile";
 import { useEffect, useState } from "react";
@@ -13,17 +13,63 @@ export default function Test() {
       <h1 className="text-xl font-bold">
         This is a basic test root, do with it what you will!
       </h1>
-      <ExampleWorkigAxiosGet />
+      <ExampleGet />
     </>
   );
 }
+
+function ExampleGet() {
+  const {
+    data: users,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: () => axios.get<UserData[]>("/user").then((res) => res.data),
+  });
+  if (isLoading) return <LoadingSpinnerMessage />;
+  if (isError) {
+    console.log(error);
+    return <p>Error fethcing data</p>;
+  }
+  return (
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.login42}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+// it's works leave it alone!
+function ExampleWorkingRactQueryWithAxiosGet() {
+  const foo = useQuery({
+    queryKey: ["users"],
+    queryFn: () => axios.get<UserData[]>("/user").then((res) => res.data),
+  });
+  if (foo.isLoading) return <LoadingSpinnerMessage />;
+  if (foo.isError) return <p>Error </p>;
+  console.log(foo.error);
+  return (
+    <ul>
+      {foo.data.map((user) => (
+        <li key={user.login42}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
+
 // don't touch it!!!!
 function ExampleWorkigAxiosGet() {
   const [posts, setPosts] = useState<UserData[]>([]);
 
   useEffect(() => {
     axios
-      .get<UserData[]>("http://localhost:3000/user/")
+      // .get<UserData[]>("http://localhost:3000/user/")
+      .get<UserData[]>("/user") // this also works because we set the other part of the url as default!
       .then((response) => {
         setPosts(response.data);
       })
@@ -35,7 +81,7 @@ function ExampleWorkigAxiosGet() {
   return (
     <ul>
       {posts.map((post) => (
-        <li key={post.login42}>{post.elo}</li>
+        <li key={post.login42}>{post.name}</li>
       ))}
     </ul>
   );
