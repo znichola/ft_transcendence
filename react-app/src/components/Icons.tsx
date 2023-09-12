b824fab36d4d015c564b9f2e96fcbe592aa6d646
BPhilagor <benjamin.philagor@gmail.com>
2023-09-12 14:46:25 +0200

    Side bar in progress

--- react-app/src/components/Icons.tsx
+++ react-app/src/components/Icons.tsx
@@ -2,7 +2,7 @@
 
 export const IconNewspaper = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -27,7 +27,7 @@ export const IconNewspaper = ({
 
 export const IconPeople = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -52,7 +52,7 @@ export const IconPeople = ({
 
 export const IconGroupChatBubble = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -77,7 +77,7 @@ export const IconGroupChatBubble = ({
 
 export const IconChatBubble = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -102,7 +102,7 @@ export const IconChatBubble = ({
 
 export const IconHomeComputer = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -127,7 +127,7 @@ export const IconHomeComputer = ({
 
 export const IconBashShell = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -152,7 +152,7 @@ export const IconBashShell = ({
 
 export const IconBolt = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -177,7 +177,7 @@ export const IconBolt = ({
 
 export const IconAtSymbol = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -201,7 +201,7 @@ export const IconAtSymbol = ({
 
 export const IconDownChevron = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -226,7 +226,7 @@ export const IconDownChevron = ({
 
 export const IconWorld = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -251,7 +251,7 @@ export const IconWorld = ({
 
 export const IconUser = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -276,7 +276,7 @@ export const IconUser = ({
 
 export const IconUserGroup = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -301,7 +301,7 @@ export const IconUserGroup = ({
 
 export const IconHeart = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -326,7 +326,7 @@ export const IconHeart = ({
 
 export const IconFire = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -356,7 +356,7 @@ export const IconFire = ({
 
 export const IconMegaphone = ({
   className = "h-5 w-5 align-middle",
-  strokeWidth = 2
+  strokeWidth = 2,
 }: {
   className?: string;
   strokeWidth?: number;
@@ -374,6 +374,56 @@ export const IconMegaphone = ({
         stroke-linecap="round"
         stroke-linejoin="round"
         d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46"
+      />
+    </svg>
+  );
+};
+
+export const IconAddUser = ({
+  className = "h-5 w-5 align-middle",
+  strokeWidth = 2,
+}: {
+  className?: string;
+  strokeWidth?: number;
+}) => {
+  return (
+    <svg
+      className={className}
+      stroke-width={strokeWidth}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+      />
+    </svg>
+  );
+};
+
+export const IconAdd = ({
+  className = "h-5 w-5 align-middle",
+  strokeWidth = 2,
+}: {
+  className?: string;
+  strokeWidth?: number;
+}) => {
+  return (
+    <svg
+      className={className}
+      stroke-width={strokeWidth}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 4.5v15m7.5-7.5h-15"
       />
     </svg>
   );
--- react-app/src/components/SideMenu.tsx
+++ react-app/src/components/SideMenu.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import {
+  IconAddUser,
+  IconAdd,
   IconAtSymbol,
   IconBashShell,
   IconBolt,
@@ -10,6 +12,7 @@ import {
   IconHomeComputer,
   IconNewspaper,
   IconPeople,
+  IconUser,
 } from "./Icons";
 import axios from "axios";
 import { UserData } from "../interfaces";
@@ -21,29 +24,46 @@ export default function SideMenu() {
   return (
     <div className="w-screen bg-slate-100">
       <div className="h-screen w-80">
-        <div className="flex h-full flex-grow flex-col overflow-y-auto rounded-br-lg rounded-tr-lg bg-white pt-5 shadow-md">
+        <div className="flex h-full flex-grow overflow-y-auto overflow-x-hidden flex-col rounded-br-lg rounded-tr-lg bg-white pt-5 shadow-md">
           <CurrentUserStats />
           <CurrentUserEloStats />
           <div className="mt-3 flex flex-1 flex-col">
             <div className="">
               <nav className="flex-1">
                 <NavSimple name="Play Pong" icon={IconHomeComputer} />
-                <NavSimple name="My Profile" icon={IconHomeComputer} />
-                <NavExpandable name="Analytics">
-                  <NavSimple name="Some Page" icon={IconBolt} />
-                  <NavSimple name="More Pages" icon={IconBashShell} />
-                  <NavSimple name="Foobar" icon={IconFire} />
-                </NavExpandable>
+                <NavSimple name="My Profile" icon={IconUser} />
               </nav>
               <Category name="Social" />
               <nav className="flex-1">
-                <NavSimple name="Messages" icon={IconChatBubble} />
-                <NavSimple name="Single Chat" icon={IconChatBubble} />
-                <NavSimple name="Orders" icon={IconGroupChatBubble} />
-                <NavSimple name="Suppliers" icon={IconPeople} />
+                <NavExpandable name="Messages" icon={IconGroupChatBubble}>
+                  <NavSimple name="Start a new conversation" icon={IconAdd}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                  <NavSimple name="User 1" icon={IconUser}/>
+                </NavExpandable>
               </nav>
-              <Category name="Content Mangement" />
-              <NavSimple name="Blog" icon={IconNewspaper} />
+              <nav>
+                <div>
+
+                </div>
+              </nav>
+              <Category name="Info" />
+              <NavSimple name="Dev log" icon={IconNewspaper} />
             </div>
             <TheMasterminds />
           </div>
@@ -87,9 +107,17 @@ function NavSimple({
 
 function NavExpandable({
   name,
+  icon : Icon,
   children,
 }: {
   name: string;
+  icon : ({
+    className,
+    strokeSize,
+  }: {
+    className?: string;
+    strokeSize?: number;
+  }) => JSX.Element;
   children?: JSX.Element[];
 }) {
   return (
@@ -101,7 +129,7 @@ function NavExpandable({
         defaultChecked={false}
       />
       <button className="peer relative flex w-full items-center border-l-rose-600 px-4 py-3 text-sm font-medium text-slate-600 outline-none transition-all duration-100 ease-in-out hover:border-l-4 hover:text-rose-600 focus:border-l-4">
-        <IconAtSymbol />
+        {Icon && <Icon />}
         <div className="w-4" />
         {name}
         <label
@@ -110,7 +138,7 @@ function NavExpandable({
         />
       </button>
       <IconDownChevron className="absolute right-0 top-4 h-4 -rotate-90 px-5 text-slate-600 transition peer-checked:rotate-90 peer-hover:text-rose-600" />
-      <ul className="duration-400 m-2 flex max-h-0 flex-col overflow-hidden rounded-xl bg-slate-100 font-medium transition-all duration-300 peer-checked:max-h-96">
+      <ul className="duration-400 m-2 flex max-h-0 flex-col overflow-y-auto rounded-xl bg-slate-100 font-medium transition-all duration-300 peer-checked:max-h-96">
         {children}
       </ul>
     </div>
