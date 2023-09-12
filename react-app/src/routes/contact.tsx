cbee19bb533c3ac3c336e9c6d61737a7ffbf8366
BPhilagor <benjamin.philagor@gmail.com>
2023-09-12 16:23:23 +0200

    Profil page cool
    Il faut encore faire propre le code mais sinon c'est cool

--- react-app/src/routes/contact.tsx
+++ react-app/src/routes/contact.tsx
@@ -1,41 +1,89 @@
 import { UserData } from "../interfaces";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { LoadingSpinnerMessage } from "../components";
 import { useQuery } from "@tanstack/react-query";
 
 export default function Contact() {
   const { login42 } = useParams<"login42">();
+  const navigate = useNavigate();
   const {
     data: user,
     isLoading,
     isError,
   } = useQuery({
     queryKey: [login42],
-    queryFn: () => axios.get<UserData>("/user/" + login42).then((res) => res.data)
+    queryFn: () =>
+      axios.get<UserData>("/user/" + login42).then((res) => res.data),
   });
 
-	if (isLoading) return <LoadingSpinnerMessage message="Loading Profile"/>
-	if (isError) return(
-			<p className="text-red-600 text-3xl">Error when loading page</p>
-	);
+  function handleHistoryClick() {
+    navigate("/users");
+  }
+
+  function handleMessageClick() {
+    navigate("/users");
+  }
+
+  function handleChallengeClick() {
+    navigate("/users");
+  }
+
+  if (isLoading) return <LoadingSpinnerMessage message="Loading Profile" />;
+  if (isError)
+    return <p className="text-3xl text-red-600">Error when loading page</p>;
 
   return (
-		<div className="flex w-[100%] h-[100%] flex-col items-center p-5 gap-5 bg-slate-200">
-			<img className="rounded-2xl w-[90%] max-h-[25%] object-cover bg-red-100 shadow-md"
-				key={user.id}
-				src={user.avatar}
-				alt={user.login42}
-			/>
+    <div className="h-[100%] w-[100%] bg-slate-200">
+      <div className="flex h-[100%] max-h-[60rem] w-[100%] max-w-3xl flex-col items-center gap-5 bg-slate-200 p-5">
+        <div className="flex max-h-[25%] w-[90%] items-center gap-3 rounded-2xl bg-slate-100 object-cover p-4 shadow-md">
+          <img
+            className="aspect-square h-[90%] rounded-full object-cover shadow-md"
+            key={user.id}
+            src={user.avatar}
+            alt={user.login42}
+          />
+          <div className="flex h-[100%] w-[100%] flex-col items-center justify-center">
+            <div className="flex grow items-center text-[300%]">
+              <p>{user.name}</p>
+            </div>
+            <div className="max-h-0 w-[100%] border-b border-slate-400 bg-slate-300"></div>
+            <div className="flex grow items-center">
+              <div className="flex h-[100%] w-[100%] items-center justify-center gap-1 text-[3rem]">
+                <p>{user.elo}</p>
+                <img
+                  className="h-[4rem] w-[4rem]"
+                  src={user.avatar}
+                  alt="elo"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
 
-			<div className="flex w-[90%] h-[25%] max-h-[25%] justify-center items-center gap-7 pt-2">
-				<div className="flex grow h-[100%] bg-teal-300 justify-center items-center rounded-md shadow-md">
-					<p className="text-3xl font-mono">Match History</p>
-				</div>
-				<div className="flex grow h-[100%] bg-lime-300 justify-center items-center rounded-md shadow-md">
-					<p className="text-3xl font-mono">Messages</p>
-				</div>
-			</div>
-		</div>
+        <div className="flex h-[25%] w-[90%] items-center justify-center gap-7 pt-2 text-white">
+          <div
+            className="flex h-[100%] grow cursor-pointer items-center justify-center rounded-md bg-indigo-700 shadow-md"
+            onClick={handleHistoryClick}
+          >
+            <p className="text-3xl font-bold">Match History</p>
+          </div>
+          <div
+            className="flex h-[100%] grow cursor-pointer items-center justify-center rounded-md bg-orange-500 shadow-md"
+            onClick={handleMessageClick}
+          >
+            <p className="text-3xl font-bold">Messages</p>
+          </div>
+        </div>
+        <div className="flex h-[25%] w-[90%] items-center justify-center gap-7 pt-2 text-white">
+          <div
+            className="flex h-[100%] grow cursor-pointer items-center justify-center rounded-md bg-amber-500 shadow-md"
+            onClick={handleChallengeClick}
+          >
+            <p className="text-5xl font-bold">Challenge</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
