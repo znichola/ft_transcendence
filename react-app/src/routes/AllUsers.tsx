e52f4c38146a0225fa6f09e7a2a92742feeca7a5
BPhilagor <benjamin.philagor@gmail.com>
2023-09-21 15:33:49 +0200

    UserInfo animation fix

--- react-app/src/routes/AllUsers.tsx
+++ react-app/src/routes/AllUsers.tsx
@@ -8,7 +8,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { useIntersection } from "../functions/uneIntersection";
 import { ReactNode, useRef } from "react";
 
-export default function AllUsers({Filter}) {
+export default function AllUsers({ Filter }) {
   const fetchPage = async ({ pageParam = 1 }) =>
     axios.get<UserData[]>("/user/?page=" + pageParam).then((res) => res.data);
   const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery({
@@ -38,7 +38,7 @@ export default function AllUsers({Filter}) {
   return (
     <>
       <div className="m-4 flex-col gap-4">
-        {_posts?.map((user) => <Filter user={user}/>)}
+        {_posts?.map((user) => <Filter user={user} />)}
       </div>
       <button
         ref={ref} // no iead why it's giving me this error but still works, for later
@@ -60,13 +60,18 @@ export default function AllUsers({Filter}) {
 export function UserInfo({ user }: { user: UserData }) {
   return (
     <div className="m-4 flex max-h-24 max-w-md bg-white shadow">
-      <div className="group flex w-20  flex-col items-center justify-center overflow-hidden border-r border-slate-200  ">
-        <div className="flex min-w-full grow flex-col items-center justify-center gap-1 overflow-hidden transition-all duration-700 group-hover:max-h-0">
+      <div className="group relative w-20 items-center justify-center overflow-hidden border-r border-slate-200  ">
+        <div className="absolute flex h-24 min-w-full grow items-center justify-center overflow-hidden duration-500 group-hover:opacity-0">
           <div className="font-bold text-slate-500">{user.elo}</div>
         </div>
-        <div className="flex h-0 min-w-full items-center justify-center overflow-hidden bg-stone-100 transition-all duration-700 group-hover:h-12 ">
-          <div className="font-bold text-green-400">{user.wins}</div>
-          <div className="text-slate-400">{user.losses}</div>
+        <div className="flex h-full w-full items-center justify-center">
+          <div className="absolute flex h-0 min-w-full flex-col items-center overflow-hidden bg-stone-100 transition-all duration-700 group-hover:h-16 ">
+            <div className="font-bold text-green-400">{user.wins + "W"}</div>
+            <div className="-translate-y-2 text-slate-400">-</div>
+            <div className="-translate-y-3 text-red-300">
+              {user.losses + "L"}
+            </div>
+          </div>
         </div>
       </div>
       <Avatar
@@ -81,32 +86,6 @@ export function UserInfo({ user }: { user: UserData }) {
         <Link to={"/user/" + user.login42} className="text-slate-400">
           {"@" + user.login42}
         </Link>
-      </div>
-    </div>
-  );
-}
-
-function FrontOfCard({ children }: { children: ReactNode }) {
-  return (
-    <div className="pointer-events-none absolute inset-0 z-20 flex h-full w-full items-center justify-center bg-white transition-all delay-200 duration-100 group-hover:opacity-0">
-      {children}
-    </div>
-  );
-}
-
-function BackOfCard({ children }: { children: ReactNode }) {
-  return (
-    <div className="card-back pointer-events-none absolute inset-0 z-10 flex h-full w-full items-center justify-center bg-stone-100 transition-all">
-      {children}
-    </div>
-  );
-}
-
-function FlipHover({ children }: { children: ReactNode }) {
-  return (
-    <div className="border-r border-slate-200">
-      <div className="card group relative h-full w-full overflow-hidden bg-pink-700 p-8 font-bold text-slate-500 transition-all duration-700">
-        {children}
       </div>
     </div>
   );
