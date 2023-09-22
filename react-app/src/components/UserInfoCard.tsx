5dd660d8099bd774692d7306a789a1f128e3b048
Nicholas Zivkovic <znichola@student.42lausanne.ch>
2023-09-22 19:03:35 +0200

    starting the nginx container as per issue #5
    this seems like an ok link? https://www.docker.com/blog/how-to-use-the-official-nginx-docker-image/
    but I can't be bothered with this right now, nothing works.
    And I'm done with it.

--- react-app/src/components/UserInfoCard.tsx
+++ react-app/src/components/UserInfoCard.tsx
@@ -13,7 +13,7 @@ export default function UserInfoCard({ user }: { user: UserData }) {
         <div className="flex h-full w-full items-center justify-start">
           <div className="duration-400 absolute flex h-full w-0 flex-col items-center justify-center overflow-hidden bg-stone-100 transition-all group-hover:w-20 group-hover:shadow-inner">
             <div className="font-bold text-green-400">{user.wins + "W"}</div>
-            <div className=" text-slate-400">—</div>
+            <div className=" text-slate-400">-</div>
             <div className=" text-red-300">{user.losses + "L"}</div>
           </div>
         </div>
@@ -59,7 +59,7 @@ export default function UserInfoCard({ user }: { user: UserData }) {
           to={to || "#"}
           className="felx-col group relative flex w-12 flex-1 items-center justify-end "
         >
-          <div className="absolute h-full grow p-1 text-slate-300 duration-300 group-hover:opacity-0 pr-2">
+          <div className="absolute h-full grow p-1 text-slate-300 duration-300 group-hover:opacity-0 pr-[0.6em]">
             {<Icon strokeWidth={2} />}
           </div>
           <div className="duration-400 absolute flex h-full w-0 items-center justify-center overflow-hidden shadow-inner rounded-l-xl bg-gradient-to-tl from-fuchsia-600 to-orange-500 transition-all group-hover:w-12 ">
@@ -81,6 +81,7 @@ export default function UserInfoCard({ user }: { user: UserData }) {
         <SideButton name={"Duel"} to="#" icon={IconBolt} />
         <SideButton name={"Chat"} to="#" icon={IconChatBubble} />
         <SideButton name={"Friend"} to="#" icon={IconAddUser} />
+        {/* to should be a put to this adress "http://localhost:3000/user/" + user.login42 + "/friends" */}
       </div>
     </div>
   );
