379ec2a05343be415defb91971a22930e53a5489
Sophia Koulen <sophia.koulen@outlook.com>
2023-09-28 18:26:24 +0200

    worked on chatroom membersoperations and error handling, but must test more thoroughly and find ways to make my code less error prone

--- nestjs/src/chat/chat.service.ts
+++ nestjs/src/chat/chat.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException, BadRequestException, ForbiddenException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AddMemberToChatroomDto } from './dto/add-member-to-chatroom-dto';
 import { CreateChatroomDto } from './dto/create-chatroom-dto';
@@ -61,12 +61,25 @@ export class ChatService
 			hash = await bcrypt.hash(chatroomDto.password, saltOrRounds);
 		}
 
-		await this.prisma.chatroom.create({
+		/* create new chatroom and return new chatroom id */
+		const newChatroom = await this.prisma.chatroom.create({
 			data: {
 				ownerId: +userId,
 				name: chatroomDto.name,
 				status: chatroomDto.status,
 				password: hash,
+			},
+			select: {
+				id: true
+			}
+		});
+
+		/* owner becomes member of chatroom */
+		await this.prisma.chatroomUser.create({
+			data: {
+				chatroomId: +newChatroom.id,
+				userId: +userId,
+				role: "OWNER"
 			}
 		});
 	}
@@ -89,6 +102,9 @@ export class ChatService
 			}
 		});
 
+		if (chatroomFromDb == null)
+			throw new NotFoundException("This chatroom does not exist");
+
 		return {
 			id: chatroomFromDb.id,
 			name: chatroomFromDb.name,
@@ -99,6 +115,22 @@ export class ChatService
 
 	async deleteChatroom(id: number)
 	{
+		await this.checkChatroomExists(id);
+
+		/* first we must delete all messages of chatroom */
+		await this.prisma.message.deleteMany({
+			where: {
+				chatroomId: +id,
+			}
+		});
+
+		/* then all members from chatroom */
+		await this.prisma.chatroomUser.deleteMany({
+			where: {
+				chatroomId: +id,
+			}
+		});
+
 		await this.prisma.chatroom.delete({
 			where: {
 				id: +id,
@@ -108,7 +140,9 @@ export class ChatService
 
 	async getOneMessageFromChatroom(id: number, msgId: number): Promise<MessageEntity>
 	{
-		const msgFromDb =  await this.prisma.message.findUnique({
+		await this.checkChatroomExists(id);
+
+		const msgFromDb = await this.prisma.message.findUnique({
 			where: {
 				id: +msgId,
 				chatroomId: +id
@@ -125,7 +159,7 @@ export class ChatService
 			}
 		});
 
-		return {
+		return (msgFromDb == null) ? null : {
 			id: msgFromDb.id,
 			senderUsername: msgFromDb.user.login42,
 			content: msgFromDb.text,
@@ -135,6 +169,8 @@ export class ChatService
 
 	async getAllMessagesFromChatroom(id: number): Promise<MessageEntity[]>
 	{
+		await this.checkChatroomExists(id);
+
 		const msgsFromDb =  await this.prisma.message.findMany({
 			where: {
 				chatroomId: +id,
@@ -228,16 +264,48 @@ export class ChatService
 
 	async updateChatroomOwner(id: number, patch: UpdateOwnerDto)
 	{
-		const userId = await this.getUserId(patch.ownerUsername);
+		const newOwnerId = await this.getUserId(patch.ownerUsername);
 
-		await this.checkIsMember(userId, id);
+		await this.checkIsMember(newOwnerId, id);
 
+		const chatroom = await this.prisma.chatroom.findUnique({
+			where: {
+				id: +id
+			},
+			select: {
+				ownerId: true
+			}
+		});
+
+		const oldOwnerId = chatroom.ownerId;
+
+		/* set the new owner as owner of the chatroom */
 		await this.prisma.chatroom.update({
 			where: {
 				id: +id,
 			},
 			data: {
-				ownerId: +userId,
+				ownerId: +newOwnerId,
+			}
+		});
+
+		/* set the new owner as OWNER */
+		await this.prisma.chatroomUser.update({
+			where: {
+				chatroomId_userId: {chatroomId: +id, userId: newOwnerId}
+			},
+			data: {
+				role: "OWNER",
+			}
+		});
+
+		/* set the old owner of the chatroom as ADMIN */
+		await this.prisma.chatroomUser.update({
+			where: {
+				chatroomId_userId: {chatroomId: +id, userId: +oldOwnerId},
+			},
+			data: {
+				role: "ADMIN"
 			}
 		});
 	}
@@ -270,6 +338,8 @@ export class ChatService
 
 	async getOneMemberFromChatroom(chatroomId: number, username: string): Promise<MemberEntity>
 	{
+		await this.checkChatroomExists(chatroomId);
+
 		const userId = await this.getUserId(username);
 
 		const memberFromDb = await this.prisma.chatroomUser.findUnique({
@@ -286,6 +356,9 @@ export class ChatService
 			}
 		});
 
+		if (memberFromDb == null)
+			throw new NotFoundException("This user is not a member of the chatroom");
+
 		return {
 			username: memberFromDb.user.login42,
 			role: memberFromDb.role,
@@ -294,6 +367,8 @@ export class ChatService
 
 	async addMemberToChatRoom(chatroomId: number, addMemberDto: AddMemberToChatroomDto)
 	{
+		await this.checkChatroomExists(chatroomId);
+
 		const userId = await this.getUserId(addMemberDto.username);
 
 		await this.prisma.chatroomUser.create({
@@ -307,7 +382,47 @@ export class ChatService
 
 	async deleteMemberFromChatRoom(chatroomId: number, member: string)
 	{
+		await this.checkChatroomExists(chatroomId);
+
 		const userId = await this.getUserId(member);
+
+		await this.checkIsMember(userId, chatroomId);
+
+		//check if user is owner
+		const chatroom = await this.prisma.chatroom.findUnique({where: {id: +chatroomId}});
+		if (chatroom.ownerId == userId)
+		{
+			/* find another member that is admin */
+			let nextOwner = await this.prisma.chatroomUser.findFirst({where: {chatroomId: +chatroomId, role: "ADMIN"}});
+			if (nextOwner == null)
+				nextOwner = await this.prisma.chatroomUser.findFirst({where: {chatroomId: +chatroomId, role: "MEMBER"}});
+
+			if (nextOwner == null)
+			{
+				await this.deleteChatroom(chatroomId);
+				return ;
+			}
+
+			/* set nextOwner as the owner of the chatroom */
+			await this.prisma.chatroom.update({
+				where: {
+					id: +chatroomId,
+				},
+				data: {
+					ownerId: +nextOwner.userId,
+				}
+			});
+
+			/* mark nextOwner's role as "OWNER" */
+			await this.prisma.chatroomUser.update({
+				where: {
+					chatroomId_userId: {chatroomId: +chatroomId, userId: +nextOwner.userId}
+				},
+				data: {
+					role: "OWNER"
+				}
+			});
+		}
 
 		await this.prisma.chatroomUser.delete({
 			where: {
@@ -318,7 +433,17 @@ export class ChatService
 
 	async updateRoleOfMemberFromChatroom(chatroomId: number, member: string, updateRoleDto: UpdateRoleDto)
 	{
+		await this.checkChatroomExists(chatroomId);
 		const userId = await this.getUserId(member);
+		await this.checkIsMember(userId, chatroomId);
+
+		if (this.isOwner(userId, chatroomId))
+			throw new ForbiddenException("Cannot change role of owner");
+
+		if (updateRoleDto.role == "OWNER")
+			throw new ForbiddenException("Role of member cannot be set to owner");
+
+		/* must access identity to check if user is allowed to perform operation (either Owner or Admin) */
 
 		await this.prisma.chatroomUser.update({
 			where: {
@@ -386,5 +511,37 @@ export class ChatService
 		{
 			throw new BadRequestException("Password validation failed");
 		}
+	}
+
+	private async checkChatroomExists(id: number)
+	{
+		const chatroom = await this.prisma.chatroom.findUnique({
+			where: {
+				id: +id,
+			},
+			select: {
+				id: true
+			}
+		});
+
+		if (chatroom == null)
+			throw new NotFoundException("This chatroom does not exist");
+	}
+
+	private async isOwner(userId: number, chatroomId: number): Promise<boolean>
+	{
+		const member = await this.prisma.chatroomUser.findUnique({
+			where: {
+				chatroomId_userId: {chatroomId: +chatroomId, userId: +userId}
+			},
+			select: {
+				role: true
+			}
+		});
+
+		if (member == null)
+			throw new NotFoundException("This user is not a member of the chatroom");
+
+		return (member.role == "OWNER");
 	}
 }
