39a50543c1ccb875008500c215b784a00e0df89c
Sophia Koulen <sophia.koulen@outlook.com>
2023-10-06 15:27:55 +0200

    Started with websockets: - There is now a /conversations Socket.IO endpoint and a /chat Socket.IO endpoint. - When there is a new direct message, a 'newDirectMessage' event is sent to `/conversations`, with as body a `ConversationEntity` that corresponds to the conversation that needs to be refreshed. - When there is a new message in a chatroom, a 'newChatroomMessage' event is sent to `/chat`, with as body a number wich is the id of the chatroom.

--- nestjs/src/dm/dm.module.ts
+++ nestjs/src/dm/dm.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DmService } from './dm.service';
 import { DmController } from './dm.controller';
+import { DmGateway } from './dm.gateway';
 
 @Module({
   controllers: [DmController],
-  providers: [DmService],
+  providers: [DmService, DmGateway],
 })
 export class DmModule {}
