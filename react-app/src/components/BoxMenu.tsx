6c8408bb63da083cae9239f3ee87c51180b071c6
Nicholas Zivkovic <znichola@student.42lausanne.ch>
2023-10-11 13:37:15 +0200

    fix #8 started #25 menu has been refactored
    - it's implemented and works for the DM menu, now for the
      chatroom menu and ranked menu, once tested #25 can be closed

---
+++ react-app/src/components/BoxMenu.tsx
@@ -0,0 +1,89 @@
+import { ReactNode, RefObject, useEffect, useRef } from "react";
+import { iconType } from "./Icons";
+
+export default function BoxMenu({
+  resetBTN,
+  heading,
+  children,
+}: {
+  resetBTN: () => void;
+  heading: ReactNode;
+  children?: ReactNode;
+}) {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  useOutsideAlerter(wrapperRef, resetBTN);
+
+  return (
+    <div
+      ref={wrapperRef}
+      className="pointer-events-none absolute top-0 w-full bg-clip-content px-28 py-5"
+    >
+      <div className="absolute left-0 top-0 z-10 h-[160%] w-full bg-gradient-to-b from-stone-50 to-transparent"></div>
+      <div className="pointer-events-auto relative z-20 flex w-full flex-col items-center justify-between rounded-xl border-b-4 border-stone-300 bg-stone-50 bg-size-200 pt-6 shadow-lg">
+        {heading}
+        <div className="flex h-16 max-h-16 gap-2 overflow-visible pb-2 pt-3">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function ButtonGeneric({
+  icon: Icon,
+  setBTNstate,
+  buttonState,
+  checked,
+  children,
+}: {
+  icon: iconType;
+  setBTNstate: (value: React.SetStateAction<string>) => void;
+  buttonState: string;
+  checked: string;
+  children?: JSX.Element[] | JSX.Element;
+}) {
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={buttonState === checked}
+        onChange={() => "UNSET"}
+        className="peer hidden"
+      />
+      <button
+        onClick={() => setBTNstate(buttonState === checked ? "UNSET" : checked)}
+        className="flex h-10 w-10 items-center justify-center rounded-full border-b-2 border-stone-300 text-slate-500 transition-all duration-100 hover:border-b-4 peer-checked:border-rose-400 peer-checked:text-rose-500"
+      >
+        <div className="flex h-8 w-8 items-center justify-center">
+          <Icon />
+        </div>
+      </button>
+      <div className="invisible absolute left-0 min-w-full translate-y-5 p-3 opacity-0 transition-all duration-500 ease-in-out peer-checked:visible peer-checked:translate-y-10 peer-checked:opacity-100">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+function useOutsideAlerter(
+  ref: RefObject<HTMLDivElement>,
+  setBTNstate: () => void,
+) {
+  useEffect(() => {
+    //  Alert if clicked on outside of element
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        ref.current &&
+        (!(event.target instanceof Node) || !ref.current.contains(event.target))
+      ) {
+        setBTNstate();
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
--- react-app/src/routes/DirectMessage.tsx
+++ react-app/src/routes/DirectMessage.tsx
@@ -1,28 +1,32 @@
 import {
   useCurrentUser,
+  useMutDeleteUserDMs,
   usePostUserConvoMessage,
   useUserConvoMessages,
   useUserData,
 } from "../functions/customHook";
-import ChatRoomMenu from "../components/ChatRoomMenu";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { LoadingSpinnerMessage } from "../components/Loading";
 import { ErrorMessage } from "../components/ErrorComponents";
 import { Message } from "../components/ChatMassages";
 import { UserData } from "../interfaces";
 import { useRef, useState } from "react";
+import BoxMenu, { ButtonGeneric } from "../components/BoxMenu";
+import { IconArrowUturnLeft } from "../components/Icons";
 
 export default function DirectMessage() {
   const scrollRef = useRef<null | HTMLDivElement>(null);
-  const { login42: target_string } = useParams<"login42">();
+  const { login42: t } = useParams<"login42">();
+  const target_string = t || "";
   const { data: user_string } = useCurrentUser();
   const { data: target, isSuccess: targetSuccess } = useUserData(target_string);
   const { data: user, isSuccess: userSuccess } = useUserData(user_string);
-  const {
-    data: messages,
-    isLoading,
-    isError,
-  } = useUserConvoMessages(user_string, target_string || "err");
+  // prettier-ignore
+  const { data: messages, isLoading, isError } = useUserConvoMessages(user_string, target_string);
+
+  // menu buttons
+  const [buttonState, setButtonState] = useState<string>("UNSET");
+
   if (isLoading)
     return <LoadingSpinnerMessage message="Loading chat history ..." />;
   if (isError || !targetSuccess || !userSuccess) {
@@ -32,7 +36,20 @@ export default function DirectMessage() {
 
   return (
     <div className="relative flex h-full max-h-full min-h-0 w-full flex-grow-0 flex-col items-center">
-      <ChatRoomMenu title={target.name} type="dm" />
+      <BoxMenu
+        resetBTN={() => setButtonState("UNSET")}
+        heading={<LeaveConversationHeading user={target} />}
+      >
+        <ButtonGeneric
+          icon={IconArrowUturnLeft}
+          setBTNstate={setButtonState}
+          buttonState={buttonState}
+          checked="LEAVE"
+        >
+          <LeaveConversationUI user1={user_string} user2={target_string} />
+        </ButtonGeneric>
+      </BoxMenu>
+
       <div className="flex h-full min-h-0 w-full flex-col gap-1 overflow-auto bg-stone-100 p-3 px-10 pb-52 pt-56">
         {messages
           .sort(
@@ -55,6 +72,51 @@ export default function DirectMessage() {
       </div>
       <MessageInput scrollRef={scrollRef} user={user} target={target} />
     </div>
+  );
+}
+
+function LeaveConversationHeading({ user }: { user: UserData }) {
+  return (
+    <div>
+      <p className="text-left font-semibold">Converstaion with</p>
+      <h1 className="bg-gradient-to-br from-fuchsia-600 to-orange-500 bg-clip-text text-center text-5xl font-semibold text-transparent">
+        {user.name}
+      </h1>
+    </div>
+  );
+}
+
+function LeaveConversationUI({
+  user1,
+  user2,
+}: {
+  user1: string;
+  user2: string;
+}) {
+  const deletConvo = useMutDeleteUserDMs(user1, user2);
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center gap-2 rounded-lg border-b-4 border-stone-200 bg-white p-3 pt-4 text-center shadow-xl">
+        <p>
+          Leave the conversation and delete all messages for <b>all</b> users,{" "}
+          <br />
+          this action is <b>permanent</b>.
+        </p>
+        <div className="flex items-center gap-2">
+          <p className="text-rose-400">Think twice before you click!</p>
+          <button
+            onClick={() => {
+              deletConvo.mutate({ user1, user2 });
+              navigate("/message");
+            }}
+            className="h-10 rounded-lg border-b-2 border-stone-300 px-4 text-slate-500 transition-all duration-100 hover:border-b-4 hover:border-rose-400 hover:text-rose-500"
+          >
+            Leave
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
