32fe83829eeb2d20b0ba8046fdf7cc5b3708d98b
Scelewyn <Scelewyn@gmail.com>
2023-10-14 14:53:28 +0200

    bugfixes to tfa enable/login functions

--- nestjs/src/tfa/tfa.controller.ts
+++ nestjs/src/tfa/tfa.controller.ts
@@ -1,8 +1,12 @@
-import { Body, ClassSerializerInterceptor, Controller, Get, HttpException, HttpStatus, Param, Post, Req, Res, UseInterceptors } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Get, HttpException, HttpStatus, Param, Post, Req, Res, UseGuards, UseInterceptors } from '@nestjs/common';
 import { TfaService } from './tfa.service';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { AuthService } from 'src/auth/auth.service';
 import { ApiTags } from '@nestjs/swagger';
+import { TfaCodeDto } from './dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { TfaGuard } from './tfa.guard';
+import { toBuffer } from 'qrcode'
 
 @ApiTags('Authentication')
 @Controller('tfa')
@@ -13,31 +17,40 @@ export class TfaController {
         private readonly authService: AuthService,
     ) {}
 
+    @UseGuards(AuthGuard)
     @Post(':username')
-    async register(@Param('username') username: string, @Res() res: Response)
+    async register(@Param('username') username: string, @Res() res: Response, @Req() req: Request)
     {
-        console.log('allo');
+        this.authService.verifyUser(username, req.cookies[process.env.COOKIE_USR].access_token);
         const { otpauthUrl } = await this.tfaService.generateTfaSecret(username);
-        return this.tfaService.pipeQrCodeStream(res, otpauthUrl);
+        const qrCode = await toBuffer(otpauthUrl);
+        res.header('Content-Type', 'image/png');
+        res.header('Content-Disposition', 'inline; filename=qr.png');
+        res.send(qrCode);
     }
 
+    @UseGuards(AuthGuard)
     @Post(':username/enable')
-    async enableTfa(@Param('username') username: string, @Body() bodyData)
+    async enableTfa(@Param('username') username: string, @Body() bodyData: TfaCodeDto, @Req() req: Request)
     {
+        this.authService.verifyUser(username, req.cookies[process.env.COOKIE_USR].access_token);
         const userSecret = await this.tfaService.getTfaSecret(username);
         const isValid = this.tfaService.isTfaCodeValid(bodyData.tfaCode, userSecret);
         if (!isValid) throw new HttpException("Invalid Authentication code.", HttpStatus.UNAUTHORIZED);
         await this.tfaService.enableTfa(username);
     }
 
+    @UseGuards(TfaGuard)
     @Post(':username/login')
-    async login(@Param('username') username: string, @Body() bodyData, @Res() res: Response)
+    async login(@Param('username') username: string, @Body() bodyData: TfaCodeDto, @Res() res: Response, @Req() req: Request)
     {
-        const isValid = this.tfaService.isTfaCodeValid(bodyData.tfaCode, username);
+        this.authService.verifyUser(username, req.cookies[process.env.COOKIE_TMP].access_token);
+        const userSecret = await this.tfaService.getTfaSecret(username);
+        const isValid = this.tfaService.isTfaCodeValid(bodyData.tfaCode, userSecret);
         if (!isValid) throw new HttpException("Invalid Authentication code.", HttpStatus.UNAUTHORIZED);
 
         const accessTokenCookie = this.authService.getUserToken(username, true);
-        res.cookie('test', accessTokenCookie, {
+        res.cookie(process.env.COOKIE_USR, accessTokenCookie, {
             domain: 'localhost',
             httpOnly: true,
             secure: false,
@@ -45,6 +58,7 @@ export class TfaController {
             maxAge: 3600000,
           });
 
-          return res.status(302).send('/ranking' );
+          res.status(201);
+          return ("OK");
     }
 }
