d41bac32539b7d82d687f9f3e9484f52cf926e5c
Sophia Koulen <sophia.koulen@outlook.com>
2023-10-20 14:30:50 +0200

    Centralized the env handling
    Note that you must delete any .env that is in the react-app or nestjs
    folders

--- Makefile
+++ Makefile
@@ -2,8 +2,16 @@
 CONTAINERS = $(shell docker ps -a -q)
 TIDY=2>/dev/null ; true
 
+COMPOSE_FILE=docker-compose-dev.yml
+ifdef TRANSCENDANCE_MODE
+	COMPOSE_FILE=docker-compose-$(TRANSCENDANCE_MODE).yml
+endif
+
 up : env
-	docker compose -f docker-compose.yml -p mastermind up --build
+	env $$(cat env/.docker.env) docker compose -f $(COMPOSE_FILE) -p mastermind up --build
+
+down:
+	env $$(cat env/.docker.env) docker compose -f $(COMPOSE_FILE) -p mastermind down
 
 fclean : env-clean
 	docker stop       $(CONTAINERS)                  $(TIDY)
@@ -17,24 +25,17 @@ re : fclean up
 ip :
 	@docker ps -q | xargs -I{} docker inspect -f '{{.Name}} {{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {}
 
-CN = nest react postgress nginx
+CN = nest react postgres nginx
 
 $(CN) :
 	docker exec -it $@ /bin/bash
 
-# https://stackoverflow.com/questions/31466428/how-to-restart-a-single-container-with-docker-compose
-# docker-compose up --detach --build $@
-
 env-clean :
-	@[ -f kickstart.env ]   && mv kickstart.env kickstart.env.old || true
-	@[ -f .env ]            && rm .env                            || true
-	@[ -f ./react-app/env ] && rm ./react-app/.env                || true
-	@[ -f ./nestjs/env ]    && rm ./nestjs/.env                   || true
-	@printf "\033[31m all .env files removed, kickstart.env renamed to \033[33mkickstart.env.old\033[0m\n"
+	@./env/clean-env.sh
 
 env-re : env-clean env
 
 env :
-	@./gen-env.sh
+	@./env/gen-env.sh
 
-.PHONY: up fclean re ip $(CN) env
+.PHONY: up fclean re ip $(CN) env env-clean env-re
---
+++ env/gen-env.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+JWT_SECRET_KICKSTART=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 48 | head -n 1)
+DATABASE_PWD_KICKSTART=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+
+gen_kickstart_template() {
+cat <<-EOF > $SCRIPT_DIR/kickstart.env
+# This is the setup for generating the .env files for the nest and react containers
+
+# if present, these value are ported over from the kickstart.env.old
+APP_NAME=${APP_NAME:="Transcendance"}
+USE_LOCAL_HOST=${USE_LOCAL_HOST:="false"}
+API_CLIENT_ID=${API_CLIENT_ID:="_________replace_with_the_API_ID_from_the_42_api_____"}
+API_CLIENT_SECRET=${API_CLIENT_SECRET:="_____replace_with_the_secret_also_from_42________"}
+COOKIE_USR=${COOKIE_USR:="userSession"}
+COOKIE_TMP=${COOKIE_TMP:="userTemp"}
+
+# auto generated on each re gen of this file
+JWT_SECRET=$JWT_SECRET_KICKSTART
+DATABASE_PWD=$DATABASE_PWD_KICKSTART
+EOF
+}
+
+if [[ -e $SCRIPT_DIR/kickstart.env ]]; then
+	echo "kickstart.env is present, check you have correctly set the 42api ID and SECRET"
+else
+
+	if [[ -e $SCRIPT_DIR/kickstart.env.old ]]; then
+		source $SCRIPT_DIR/kickstart.env.old
+	fi
+	printf "\n\33[96m fillout the kickstart.env file with information from the 42 api!\033[0m\n\n"
+	gen_kickstart_template
+	exit 42
+fi
+
+source $SCRIPT_DIR/kickstart.env
+
+IP_ADDR=$(ipconfig getifaddr en0)
+if [[ $USE_LOCAL_HOST == "true" ]]; then
+	IP_ADDR="localhost"
+fi
+
+VITE_IP_ADDR=$IP_ADDR
+
+{
+cat <<-EOF > $SCRIPT_DIR/.react.env
+VITE_IP_ADDR=$VITE_IP_ADDR
+EOF
+}
+
+{
+cat <<-EOF > $SCRIPT_DIR/.nestjs.env
+IP_ADDR=$VITE_IP_ADDR
+API_CLIENT_ID=$API_CLIENT_ID
+API_CLIENT_SECRET=$API_CLIENT_SECRET
+JWT_SECRET=$JWT_SECRET
+COOKIE_USR=$COOKIE_USR
+COOKIE_TMP=$COOKIE_TMP
+APP_NAME=$APP_NAME
+DATABASE_URL=postgresql://postgres:$DATABASE_PWD@postgres:5432/postgres?schema=public
+EOF
+}
+
+{
+cat <<-EOF > $SCRIPT_DIR/.docker.env
+DATABASE_PWD=$DATABASE_PWD
+EOF
+}
