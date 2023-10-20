f68988491340d0d4ce33d09236921a722e361029
Scelewyn <Scelewyn@gmail.com>
2023-10-21 00:31:45 +0200

    Updated pong module imports

--- nestjs/src/pong/pong.module.ts
+++ nestjs/src/pong/pong.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PongGateway } from './pong.gateway';
 import { PongService } from './pong.service';
+import { UserModule } from 'src/user/user.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule],
   controllers: [],
   providers: [PongGateway, PongService],
 })
