bf94ef689a2f2049a46bf11372802159f215cced
Sophia Koulen <sophia.koulen@outlook.com>
2023-10-30 14:36:25 +0100

    added class validation for /user Dto's

--- nestjs/src/user/dto/user.profile.dto.ts
+++ nestjs/src/user/dto/user.profile.dto.ts
@@ -1,15 +1,17 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
 
 export class UserProfileDto {
     @ApiProperty()
     @IsString()
     @IsNotEmpty()
     @MaxLength(20)
-    name: string;
-    
+	@IsOptional()
+    name?: string;
+
     @ApiProperty()
     @IsString()
     @MaxLength(140)
-    bio: string;
+	@IsOptional()
+    bio?: string;
 }
